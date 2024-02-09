fe7dd81f15090a57aa6f2f8c2e442f92784c6dc8
Leo <eta.development.leo@outlook.com>
2024-02-09 11:13:16 +0300

    Init

---
+++ lib/access_token_dialog.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class AccessTokenDialog extends AlertDialog {
+  final BuildContext context;
+
+  @override
+  EdgeInsetsGeometry? get titlePadding => const EdgeInsets.fromLTRB(20, 15, 20, 15);
+
+  const AccessTokenDialog({super.key, required this.context});
+
+  @override
+  Widget get title => Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          const Expanded(child: Text('Create access token')),
+          IconButton(
+              icon: const Icon(Icons.close),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+        ],
+      );
+
+  @override
+  get shape => const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10.0)));
+
+  @override
+  List<Widget> get actions => [
+        ElevatedButton.icon(
+          label: const Text('OK'),
+          onPressed: () {
+            debugPrint('test');
+            Navigator.pop(context);
+          },
+          style: ElevatedButton.styleFrom(
+            shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
+          ),
+          icon: const Icon(Icons.close),
+        )
+      ];
+
+  @override
+  Widget get content => SizedBox(
+        width: MediaQuery.of(context).size.width * 0.4,
+        child: const Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [Text('Custom Content')],
+        ),
+      );
+}
